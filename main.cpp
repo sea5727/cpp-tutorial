197b99323c2e5a56da9113dfd57d0ae5b25cd173
ysh8361 <sea5727@naver.com>
2021-01-27 20:56:30 +0900

    commit

--- main.cpp
+++ main.cpp
@@ -1,9 +1,30 @@
 #include <iostream>
 #include <array>
+#include <functional>
+#include <stdlib.h>
+#include <stdarg.h>
+
+void test2(const char * fmt, va_list ap){
+    std::function<void()> f = [=]{
+        
+        char buf[1024];
+        vsprintf(buf, fmt, ap);
+        printf("f start..buf:%s\n", buf);
+    };
+    f();
+}
 
 
+void test(const char * fmt, ...){
+    va_list ap;
+    va_start(ap, fmt);
+    test2(fmt, ap);
+    va_end(ap);
+}
 
 int main(int, char**) {
+    test("%s %s %s", "HGET" , "KETS" , "TEST");
+
     std::cout << "Hello, world!\n";
     int n = 100;
 
