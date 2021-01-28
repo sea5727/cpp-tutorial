3cf09ef5e559632637b9d63c7f79d2502aa1c029
ysh8361 <sea5727@naver.com>
2021-01-28 20:26:50 +0900

    commit

---
+++ move_forward/basic_tutorial_2/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+MyClass{
+public:
+    std::function<void()> f0;
+    std::function<void()> & f1;
+    std::function<void()> && f2;
+    
+}
+int main() {
+    
+
+    int y = 100;
+    testfunc(y);
+
+}
+
